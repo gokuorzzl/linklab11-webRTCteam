e5789b3a0446d01abdfe46c02f740faaacad3e31
cheolwoo <y2456y16@gmail.com>
2017-07-15 14:02:23 +0900

    문자전송 완료 gps만 계속변경이되어 바뀌도록 바꾸는것만 남음

--- Android/studentapp/src/main/java/com/example/studentapp/MainActivity.java
+++ Android/studentapp/src/main/java/com/example/studentapp/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.studentapp;
+
 import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
@@ -11,7 +12,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
@@ -19,35 +19,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     //화면 세팅
-    private Button btn_start, btn_stop;
+    private Button sms_send, btn_stop;
     private TextView textView;
-    private TextView smsNumber;
-    private Context mContext = null;
     private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(broadcastReceiver == null){
+        if (broadcastReceiver == null) {
             broadcastReceiver = new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context context, Intent intent) {
 
-                    textView.append("\n" +intent.getExtras().get("coordinates"));
+                    textView.append("\n" + intent.getExtras().get("coordinates"));
 
                 }
             };
         }
-        registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+        registerReceiver(broadcastReceiver, new IntentFilter("location_update"));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(broadcastReceiver != null){
+        if (broadcastReceiver != null) {
             unregisterReceiver(broadcastReceiver);
         }
     }
@@ -58,12 +56,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_start = (Button) findViewById(R.id.button);
+        sms_send = (Button) findViewById(R.id.button);
         btn_stop = (Button) findViewById(R.id.button2);
         textView = (TextView) findViewById(R.id.textView);
-        smsNumber = (TextView) findViewById(R.id.smsNumber);
 
-        if(!runtime_permissions())
+
+        if (!runtime_permissions())
             enable_buttons();
 
     }
@@ -71,18 +69,16 @@ public class MainActivity extends AppCompatActivity {
     //버튼 누를시 gps 정보 받아오도록 하는 코드
     private void enable_buttons() {
 
-        btn_start.setOnClickListener(new View.OnClickListener() {
+        Intent i = new Intent(getApplicationContext(), GpsInfo.class);
+        startService(i);
+
+        sms_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i =new Intent(getApplicationContext(),GpsInfo.class);
+                Intent i = new Intent(getApplicationContext(), GpsInfo.class);
                 startService(i);
-                String smsNum = smsNumber.getText().toString();
-                String smsText = textView.getText().toString();
-                if(smsNum.length()>0 && smsText.length()>0){
-                    sendSMS(smsNum, smsText);
-                }else{
-                    Toast.makeText(mContext, "모두 입력해 주세요", Toast.LENGTH_SHORT).show();
-                }
+                String location = textView.getText().toString();
+                sendSMS("01047199044", location);
             }
         });
 
@@ -90,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent(getApplicationContext(),GpsInfo.class);
+                Intent i = new Intent(getApplicationContext(), GpsInfo.class);
                 stopService(i);
 
             }
@@ -100,9 +96,9 @@ public class MainActivity extends AppCompatActivity {
 
     //안드로이드 버전 6이상부터는 이러한 허가가 있어야 gps를 받아올수 있음
     private boolean runtime_permissions() {
-        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100);
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
 
             return true;
         }
@@ -114,66 +110,77 @@ public class MainActivity extends AppCompatActivity {
     //여기서 미리 사용자에게 요청했던 값을 반환하여 허락이면 그대로 실행 아니면 다시 허락요청
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 100){
-            if( grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                 enable_buttons();
-            }else {
+            } else {
                 runtime_permissions();
             }
         }
     }
 
     //문자 전송
-    public void sendSMS(String smsNumber, String smsText){
-        PendingIntent sentIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT_ACTION"), 0);
-        PendingIntent deliveredIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED_ACTION"), 0);
+    private void sendSMS(String phoneNumber, String message) {
 
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+
+        // 문자 보내는 상태를 감지하는 PendingIntent
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+        // 문자 받은 상태를 감지하는 PendingIntent
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
+
+
+        // 문자 보내는 상태를 감지하는 BroadcastReceiver를 등록한다.
         registerReceiver(new BroadcastReceiver() {
+
+            // 문자를 수신하면, 발생.
             @Override
             public void onReceive(Context context, Intent intent) {
-                switch(getResultCode()){
+                switch (getResultCode()) {
                     case Activity.RESULT_OK:
-                        // 전송 성공
-                        Toast.makeText(mContext, "전송 완료", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "SMS sent", Toast.LENGTH_SHORT).show();
                         break;
                     case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        // 전송 실패
-                        Toast.makeText(mContext, "전송 실패", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "Generic failure", Toast.LENGTH_SHORT).show();
                         break;
                     case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        // 서비스 지역 아님
-                        Toast.makeText(mContext, "서비스 지역이 아닙니다", Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        // 무선 꺼짐
-                        Toast.makeText(mContext, "무선(Radio)가 꺼져있습니다", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "No service", Toast.LENGTH_SHORT).show();
                         break;
                     case SmsManager.RESULT_ERROR_NULL_PDU:
-                        // PDU 실패
-                        Toast.makeText(mContext, "PDU Null", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "Null PDU", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
-        }, new IntentFilter("SMS_SENT_ACTION"));
+        }, new IntentFilter(SENT));
 
+        // 문자를 받는 상태를 확인하는 BroadcastReceiver를 등록.
         registerReceiver(new BroadcastReceiver() {
+
+
+            // 문자를 받게 되면, 불린다.
             @Override
             public void onReceive(Context context, Intent intent) {
-                switch (getResultCode()){
+                switch (getResultCode()) {
                     case Activity.RESULT_OK:
-                        // 도착 완료
-                        Toast.makeText(mContext, "SMS 도착 완료", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "SMS delivered", Toast.LENGTH_SHORT).show();
                         break;
                     case Activity.RESULT_CANCELED:
-                        // 도착 안됨
-                        Toast.makeText(mContext, "SMS 도착 실패", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "SMS not delivered", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
-        }, new IntentFilter("SMS_DELIVERED_ACTION"));
+        }, new IntentFilter(DELIVERED));
 
-        SmsManager mSmsManager = SmsManager.getDefault();
-        mSmsManager.sendTextMessage(smsNumber, null, smsText, sentIntent, deliveredIntent);
+
+        // SmsManager를 가져온다.
+        SmsManager sms = SmsManager.getDefault();
+        // sms를 보낸다.
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
 
     }
 }
