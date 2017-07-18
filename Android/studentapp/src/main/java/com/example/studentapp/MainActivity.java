37323ffdb080d59349598eed4caf5ca754615e26
cheolwoo <y2456y16@gmail.com>
2017-07-18 16:50:37 +0900

    no message

--- Android/studentapp/src/main/java/com/example/studentapp/MainActivity.java
+++ Android/studentapp/src/main/java/com/example/studentapp/MainActivity.java
@@ -8,10 +8,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
@@ -19,103 +25,82 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     //화면 세팅
-    private Button sms_send, btn_stop;
+    private Button sms_send;
     private TextView textView;
-    private BroadcastReceiver broadcastReceiver;
+    private LocationManager locationManager;
+    private LocationListener listener;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (broadcastReceiver == null) {
-            broadcastReceiver = new BroadcastReceiver() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-
-                    textView.append("\n" + intent.getExtras().get("coordinates"));
-
-                }
-            };
-        }
-        registerReceiver(broadcastReceiver, new IntentFilter("location_update"));
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (broadcastReceiver != null) {
-            unregisterReceiver(broadcastReceiver);
-        }
-    }
-
-    @Override
     //버튼 및 글 생성
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         sms_send = (Button) findViewById(R.id.button);
-        btn_stop = (Button) findViewById(R.id.button2);
         textView = (TextView) findViewById(R.id.textView);
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        if (!runtime_permissions())
-            enable_buttons();
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                textView.append("\n" + location.getLongitude() + " " + location.getLatitude());
+            }
 
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        enable_buttons();
     }
 
     //버튼 누를시 gps 정보 받아오도록 하는 코드
-    private void enable_buttons() {
 
-        Intent i = new Intent(getApplicationContext(), GpsInfo.class);
-        startService(i);
+    void enable_buttons() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,100);
+            }
+            return;
+        }
+        locationManager.requestLocationUpdates("gps", 5000, 0, listener);
 
         sms_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), GpsInfo.class);
-                startService(i);
+
                 String location = textView.getText().toString();
                 sendSMS("01047199044", location);
             }
         });
-
-        btn_stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent i = new Intent(getApplicationContext(), GpsInfo.class);
-                stopService(i);
-
-            }
-        });
-
     }
 
-    //안드로이드 버전 6이상부터는 이러한 허가가 있어야 gps를 받아올수 있음
-    private boolean runtime_permissions() {
-        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
-
-            return true;
-        }
-        return false;
-    }
-
-
-    @Override
     //여기서 미리 사용자에게 요청했던 값을 반환하여 허락이면 그대로 실행 아니면 다시 허락요청
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 100) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+        switch (requestCode){
+            case 10:
                 enable_buttons();
-            } else {
-                runtime_permissions();
-            }
+                break;
+            default:
+                break;
         }
     }
 
