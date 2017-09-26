e649c2ba174cf74fd8ecd977f8379da5a260d982
DESKTOP-BAFSCHJ\KimMinYoung <goldenwatchplus@gmail.com>
2017-09-26 18:17:12 +0900

    .

--- ChannelTest/app/src/main/java/com/example/kimminyoung/channeltest/MainActivity.java
+++ ChannelTest/app/src/main/java/com/example/kimminyoung/channeltest/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.kimminyoung.channeltest;
 
 import android.content.DialogInterface;
-import android.graphics.drawable.PaintDrawable;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.content.Intent;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,8 +18,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -43,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         listView = (ListView) findViewById(R.id.list);
